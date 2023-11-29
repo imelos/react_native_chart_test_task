4c578e32a87ccd982d0c6528dd8b00af46be62cf
imelos <imelose@gmail.com>
2023-11-30 02:04:20 +0400

    +

--- components/details-list/index.js
+++ components/details-list/index.js
@@ -4,13 +4,12 @@ import DetailsListItem from './details-list-item';
 import {Divider} from '@react-native-material/core';
 
 export default function DetailsList(props) {
-  console.log(props.data)
-  console.log("data")
   return (
     <View>
       {props.data.map((item, index) => {
         return (
-          <View key={item.id}>
+          //sorry for using index, this items are not going for reorder, so i can use it here.
+          <View key={index}>
             {index !== 0 ? <Divider /> : null}
             <DetailsListItem data={item} />
           </View>
